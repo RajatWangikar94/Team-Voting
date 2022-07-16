c8177b1b378c9d36fe8e945759eebe67b7f444a9
RajatWangikar94 <84658365+RajatWangikar94@users.noreply.github.com>
2022-07-16 06:14:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB7p1lTPtkiuMc6fWeRY1fc7ZLqTSbDPto",
+  authDomain: "team-voting-cf6f7.firebaseapp.com",
+  databaseURL: "https://team-voting-cf6f7-default-rtdb.firebaseio.com",
+  projectId: "team-voting-cf6f7",
+  storageBucket: "team-voting-cf6f7.appspot.com",
+  messagingSenderId: "449227776370",
+  appId: "1:449227776370:web:2b0808d37c7f33c2597deb"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
